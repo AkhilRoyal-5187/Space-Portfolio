eb25ac04d0fa2247fc0ae051c657834725ffd5cb
AKHIL ROYAL <akhilroyal5187@gmail.com>
2025-04-19 13:07:31 +0530

    Add files via upload
    This is 1st commit as GUI and 2n to the Git

---
+++ space-portfolio/constants/index.ts
@@ -0,0 +1,289 @@
+import { FaYoutube, FaFacebook } from "react-icons/fa";
+import {
+  RxDiscordLogo,
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxTwitterLogo,
+  RxLinkedinLogo,
+} from "react-icons/rx";
+
+export const SKILL_DATA = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+ 
+  
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+  
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+] as const;
+
+export const SOCIALS = [
+  {
+    name: "Instagram",
+    icon: RxInstagramLogo,
+    link: "https://www.instagram.com/akhilroyal5187/profilecard/?igsh=emNkeTh6dGZ3MHhj",
+  },
+
+  {
+    name: "Linkdin",
+    icon : RxLinkedinLogo,
+    link: "https://www.linkedin.com/in/akhilroyal5187/",
+  },
+] as const;
+
+export const FRONTEND_SKILL = [
+  {
+    skill_name: "HTML",
+    image: "html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "CSS",
+    image: "css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "JavaScript",
+    image: "js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    skill_name: "Tailwind CSS",
+    image: "tailwind.png",
+    width: 80,
+    height: 80,
+  },
+ 
+  {
+    skill_name: "React",
+    image: "react.png",
+    width: 80,
+    height: 80,
+  },
+  
+  
+  {
+    skill_name: "TypeScript",
+    image: "ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Next.js 14",
+    image: "next.png",
+    width: 80,
+    height: 80,
+  },
+] as const;
+
+export const BACKEND_SKILL = [
+  {
+    skill_name: "Node.js",
+    image: "node.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "Express.js",
+    image: "express.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    skill_name: "MongoDB",
+    image: "mongodb.png",
+    width: 40,
+    height: 40,
+  },
+  {
+    skill_name: "Firebase",
+    image: "firebase.png",
+    width: 55,
+    height: 55,
+  },
+  
+  {
+    skill_name: "MySQL",
+    image: "mysql.png",
+    width: 70,
+    height: 70,
+  },
+  
+] as const;
+
+export const FULLSTACK_SKILL = [
+  {
+    skill_name: "React Native",
+    image: "reactnative.png",
+    width: 70,
+    height: 70,
+  },
+  {
+    skill_name: "Docker",
+    image: "docker.png",
+    width: 70,
+    height: 70,
+  },
+
+] as const;
+
+export const OTHER_SKILL = [
+ 
+] as const;
+
+export const PROJECTS = [
+  {
+    title: " Food Delivery Landing Page",
+    description:
+      'This food delivery landing page showcases a light theme with a clean, modern design. Built with HTML, CSS, and JavaScript, and styled using Tailwind CSS, it offers a sleek and efficient user interface. The page features engaging animations, vibrant visuals of food, and prominent call-to-action buttons to guide users. Its designed to provide a professional and inviting experience, making it perfect for food service businesses looking to attract and engage customers.',
+    image: "/projects/project-01.png",
+    link: "https://dancing-syrniki-c21ef8.netlify.app/",
+  },
+  {
+    title: "Amazon Clone",
+    description:
+      'A full-stack Amazon clone built using React, Tailwind CSS, Node.js, and MongoDB. This project replicates key features of Amazon, including user authentication, product listings, shopping cart, and order management. With a responsive UI and fully integrated backend, it offers a complete e-commerce experience from browsing to checkout.',
+    image: "/projects/project02.png",
+    link: "https://github.com/AkhilRoyal-5187/Codedale-Assignments",
+  },
+  {
+    title: "TIC TAC TOE",
+    description:
+      'A classic Tic Tac Toe game built using React, Tailwind CSS, and JavaScript. This interactive project features a clean UI, smooth game logic, and real-time win detection. Players can enjoy a seamless two-player experience with instant feedback and a reset option — all on a responsive and minimal interface.',
+    image: "/projects/project03.png",
+    link: "https://timely-cassata-3c7181.netlify.app/",
+  },
+] as const;
+
+export const FOOTER_DATA = [
+  // {
+  //   title: "Community",
+  //   data: [
+  //     {
+  //       name: "YouTube",
+  //       icon: FaYoutube,
+  //       link: "https://youtube.com",
+  //     },
+  //     {
+  //       name: "GitHub",
+  //       icon: RxGithubLogo,
+  //       link: "https://github.com/AkhilRoyal-5187/",
+  //     },
+      
+  //   ],
+  // },
+  {
+    title: "Social Media",
+    data: [
+      {
+        name: "Instagram",
+        icon: RxInstagramLogo,
+        link: "https://www.instagram.com/akhilroyal5187/profilecard/?igsh=emNkeTh6dGZ3MHhjhttps://www.instagram.com/akhilroyal5187/profilecard/?igsh=emNkeTh6dGZ3MHhj",
+      },
+      {
+        name: "Twitter",
+        icon: RxTwitterLogo,
+        link: "https://x.com/royal49554?t=Kn5bjnISdedt7u3FqVzWVw&s=09",
+      },
+      {
+        name: "Linkedin",
+        icon: RxLinkedinLogo,
+        link: "www.linkedin.com/in/akhilroyal5187",
+      },
+      {
+        name: "GitHub",
+        icon: RxGithubLogo,
+        link: "https://github.com/AkhilRoyal-5187/",
+      },
+      {
+        name: "Mail",
+        icon: null,
+        link: "mailto:akhilroyal5187@gmail.com",
+      },
+    ],
+  },
+  // {
+  //   title: "About",
+  //   data: [
+      
+  //     {
+  //       name: "Contact Me",
+  //       icon: null,
+  //       link: "mailto:akhilroyal5187@gmail.com",
+  //     },
+  //   ],
+  // },
+] as const;
+
+export const NAV_LINKS = [
+  {
+    title: "About me",
+    link: "#about-me",
+  },
+  {
+    title: "Skills",
+    link: "#skills",
+  },
+  {
+    title: "Projects",
+    link: "#projects",
+  },
+  
+] as const;
+
+export const LINKS = {
+  sourceCode: "https://drive.google.com/file/d/1af_wrhWe1GRHJla7C_K3gLrthIbAWJVF/view?usp=drivesdk",
+};
